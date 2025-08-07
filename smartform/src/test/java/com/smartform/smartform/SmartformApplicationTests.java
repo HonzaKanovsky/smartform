4c7bd47afec592d685476c3392351d8978c579bf
Jan Kanovsky <honza@kanovsky.net>
2025-08-07 12:32:59 +0200

    project init

---
+++ smartform/src/test/java/com/smartform/smartform/SmartformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartform.smartform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
